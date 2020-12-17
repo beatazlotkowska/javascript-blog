862452f7c2d7e5985e3bd99f4d5e304d0810f130
Beata Zlotkowska <beata.zlotkowska94@gmail.com>
2020-12-17 20:53:22 +0100

    fix js file

--- js/script.js
+++ js/script.js
@@ -8,23 +8,34 @@
 // const links = document.querySelectorAll('.titles a');
 
 const titleClickHandler = function(event){
-  console.log('Link was clicked!');
-  console.log(event);
-  /* remove class 'active' from all article links  */
+	console.log('Link was clicked!');
+	console.log(event);
+	/* remove class 'active' from all article links  */
+	const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* add class 'active' to the clicked link */
+	for(let activeLink of activeLinks){
+		activeLink.classList.remove('active');
+	}
+	/* add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+	/* remove class 'active' from all articles */
+	
+	const activeArticles = document.querySelectorAll('.posts .post.active');
 
-  /* get 'href' attribute from the clicked link */
+	for(let activeArticle of activeArticles){
+	 	activeArticle.classList.remove('active');
+	 }  
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+	/* get 'href' attribute from the clicked link */
 
-  /* add class 'active' to the correct article */
+	/* find the correct article using the selector (value of 'href' attribute) */
+
+	/* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+	link.addEventListener('click', titleClickHandler);
+
 }
