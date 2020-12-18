65afbbbe617f4fb5b84b52c353524f75947748cb
Beata Zlotkowska <beata.zlotkowska94@gmail.com>
2020-12-18 22:03:40 +0100

    add class active

--- js/script.js
+++ js/script.js
@@ -8,17 +8,20 @@
 // const links = document.querySelectorAll('.titles a');
 
 const titleClickHandler = function(event){
+	const clickedElement = this;
 	console.log('Link was clicked!');
 	console.log(event);
-	/* remove class 'active' from all article links  */
+	/* [DONE] remove class 'active' from all article links  */
 	const activeLinks = document.querySelectorAll('.titles a.active');
 
 	for(let activeLink of activeLinks){
 		activeLink.classList.remove('active');
 	}
-	/* add class 'active' to the clicked link */
-
-	/* remove class 'active' from all articles */
+	/* [IN PROGRESS] add class 'active' to the clicked link */
+	 console.log('clickedElement:' , clickedElement);
+	 clickedElement.classList.add('active');
+	
+	/* [DONE] remove class 'active' from all articles */
 	
 	const activeArticles = document.querySelectorAll('.posts .post.active');
 
@@ -31,6 +34,8 @@ const titleClickHandler = function(event){
 	/* find the correct article using the selector (value of 'href' attribute) */
 
 	/* add class 'active' to the correct article */
+	
+	
 }
 
 const links = document.querySelectorAll('.titles a');
